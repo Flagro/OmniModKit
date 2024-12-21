0f0da51b1f6abe5d7a3df5479426eb0ad0a521d7
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-21 14:18:48 +0100

    Do not use models toolkit in utilities

--- omnimodkit/models_tools/base_utility.py
+++ omnimodkit/models_tools/base_utility.py
@@ -1,18 +1,7 @@
 from abc import ABC, abstractmethod
 
-from ..models_toolkit import ModelsToolkit
-
 
 class BaseUtility(ABC):
-    name: str
-    description: str
-
-    def __init__(
-        self,
-        models_toolkit: ModelsToolkit,
-    ):
-        self.models_toolkit = models_toolkit
-
     @abstractmethod
     async def run(self, *args, **kwargs):
         raise NotImplementedError
