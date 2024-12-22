2846fd2aca714868f26ab97c03e19ca97b97bbbd
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-22 14:22:12 +0100

    Replace the utility logic with models logic

--- omnimodkit/models_tools/image_generation_model/describe_image.py
+++ omnimodkit/models_tools/image_generation_model/describe_image.py
@@ -1,24 +1,30 @@
 import io
+import base64
 from typing import Type, Optional
 from langchain_core.pydantic_v1 import BaseModel
 from langchain_core.output_parsers import JsonOutputParser
 
-from .base_utility import BaseUtility
-from ..prompt_manager import PromptManager
+from ...prompt_manager import PromptManager
 
 
-class DescribeAudioUtililty(BaseUtility):
+class DescribeImageUtililty:
     def run(
         self,
         in_memory_image_stream: io.BytesIO,
         pydantic_object: Optional[Type[BaseModel]] = None,
     ) -> BaseModel:
         if pydantic_object is None:
-            # TODO: prompt manager should be available as part of the utility
-            pydantic_object = PromptManager.get_default_audio_information()
+            pydantic_object = PromptManager.get_default_image_information()
         # Encode in base64:
+        image_base64 = base64.b64encode(in_memory_image_stream.getvalue()).decode()
         parser = JsonOutputParser(pydantic_object=pydantic_object)
-        return pydantic_object(audio_description="an audio")
+
+        # TODO: implement image chain runnable
+        # TODO: pass the image model here
+
+        return pydantic_object(
+            image_description="an image", image_type="picture", main_objects=["image"]
+        )
 
     async def arun(
         self,
