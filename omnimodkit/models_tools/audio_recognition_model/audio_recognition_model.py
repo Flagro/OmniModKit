575b1b98aaea4d0ccbe4f750842cb56082cc13d3
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-23 14:05:12 +0100

    Merge audio and image utilities with model classes

--- omnimodkit/models_tools/audio_recognition_model/audio_recognition_model.py
+++ omnimodkit/models_tools/audio_recognition_model/audio_recognition_model.py
@@ -1,6 +1,32 @@
+import io
+from typing import Type, Optional
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.output_parsers import JsonOutputParser
+
 from ..base_model_toolkit import BaseModelToolkit
+from ...prompt_manager import PromptManager
 
 
 class AudioRecognitionModel(BaseModelToolkit):
     def __init__(self, model):
         self.client = model
+
+    def run(
+        self,
+        in_memory_image_stream: io.BytesIO,
+        pydantic_object: Optional[Type[BaseModel]] = None,
+    ) -> BaseModel:
+        if pydantic_object is None:
+            # TODO: prompt manager should be available as part of the utility
+            pydantic_object = PromptManager.get_default_audio_information()
+        # Encode in base64:
+        parser = JsonOutputParser(pydantic_object=pydantic_object)
+        return pydantic_object(audio_description="an audio")
+
+    async def arun(
+        self,
+        in_memory_image_stream: io.BytesIO,
+        pydantic_object: Optional[Type[BaseModel]] = None,
+    ) -> BaseModel:
+        # TODO: make it non-blocking
+        return self.run(in_memory_image_stream, pydantic_object)
--- omnimodkit/models_tools/image_generation_model/image_generation_model.py
+++ omnimodkit/models_tools/image_generation_model/image_generation_model.py
@@ -1,6 +1,39 @@
+import io
+import base64
+from typing import Type, Optional
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.output_parsers import JsonOutputParser
+
 from ..base_model_toolkit import BaseModelToolkit
+from ...prompt_manager import PromptManager
 
 
 class ImageGenerationModel(BaseModelToolkit):
     def __init__(self, model):
         self.client = model
+
+    def run(
+        self,
+        in_memory_image_stream: io.BytesIO,
+        pydantic_object: Optional[Type[BaseModel]] = None,
+    ) -> BaseModel:
+        if pydantic_object is None:
+            pydantic_object = PromptManager.get_default_image_information()
+        # Encode in base64:
+        image_base64 = base64.b64encode(in_memory_image_stream.getvalue()).decode()
+        parser = JsonOutputParser(pydantic_object=pydantic_object)
+
+        # TODO: implement image chain runnable
+        # TODO: pass the image model here
+
+        return pydantic_object(
+            image_description="an image", image_type="picture", main_objects=["image"]
+        )
+
+    async def arun(
+        self,
+        in_memory_image_stream: io.BytesIO,
+        pydantic_object: Optional[Type[BaseModel]] = None,
+    ) -> BaseModel:
+        # TODO: make it non-blocking
+        return self.run(in_memory_image_stream, pydantic_object)
