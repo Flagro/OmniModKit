733bf3877beed3121d5127459eda919413cb93b6
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-27 13:32:49 +0100

    Add model_name attribute to BaseModelToolkit for enhanced model identification

--- omnimodkit/models_tools/base_model_toolkit.py
+++ omnimodkit/models_tools/base_model_toolkit.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class BaseModelToolkit(ABC):
+    model_name: str
+
     @abstractmethod
     def get_price(*args, **kwargs):
         raise NotImplementedError
