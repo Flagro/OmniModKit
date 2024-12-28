fa0f605387280d017d2c3a5dbf4b86ccd76d73b1
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-12-28 22:42:18 +0100

    Add model imports for audio, image, text, and vision models in models_tools

---
+++ omnimodkit/models_tools/__init__.py
@@ -0,0 +1,12 @@
+from .audio_recognition_model.audio_recognition_model import (
+    AudioRecognitionModel,
+)
+from .image_generation_model.image_generation_model import (
+    ImageGenerationModel,
+)
+from .text_model.text_model import (
+    TextModel,
+)
+from .vision_model.vision_model import (
+    VisionModel,
+)
