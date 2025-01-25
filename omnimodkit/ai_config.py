83fbe25e833faf0ef6d285aca7c00bb3d144f92a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2025-01-25 23:34:10 +0100

    Remove redundant request_timeout parameter from AIConfig YAML and update model classes to use default value

--- omnimodkit/ai_config.py
+++ omnimodkit/ai_config.py
@@ -14,6 +14,7 @@ class Model(BaseModel):
     name: str
     temperature: float
     structured_output_max_tokens: int = 1024
+    request_timeout: float = 60
     text_default: Optional[bool] = False
     vision_default: Optional[bool] = False
     image_generation_default: Optional[bool] = False
@@ -26,20 +27,17 @@ class TextGeneration(BaseModel):
     top_p: int
     frequency_penalty: int
     presence_penalty: int
-    request_timeout: float
     Models: Dict[str, Model]
 
 
 class ImageGeneration(BaseModel):
     moderation_needed: bool = True
     output_image_size: str
-    request_timeout: float
     Models: Dict[str, Model]
 
 
 class AudioRecognition(BaseModel):
     moderation_needed: bool = True
-    request_timeout: float
     Models: Dict[str, Model]
 
 
