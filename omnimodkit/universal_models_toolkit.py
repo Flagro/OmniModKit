060a0622d71253d4693c807b1c49635813adcef5
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2025-02-09 15:36:41 +0100

    Add get_text_response method to UniversalModelsToolkit for handling user input

--- omnimodkit/universal_models_toolkit.py
+++ omnimodkit/universal_models_toolkit.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from pydantic import BaseModel
 from .ai_config import AIConfig
 from .models_tools import (
     TextModel,
@@ -6,6 +8,7 @@ from .models_tools import (
     AudioRecognitionModel,
 )
 from .models_toolkit import ModelsToolkit
+from .prompt_manager import PromptManager
 
 
 class UniversalModelsToolkit:
@@ -20,3 +23,11 @@ class UniversalModelsToolkit:
                 AudioRecognitionModel,
             ],
         )
+
+    def get_text_response(
+        self, user_input: str, system_prompt: Optional[str] = None
+    ) -> BaseModel:
+        if system_prompt is None:
+            system_prompt = PromptManager.get_default_system_prompt_text()
+        messages = TextModel.compose_messages_openai(user_input, system_prompt)
+        return self.models_toolkit.run_model("text", messages)
